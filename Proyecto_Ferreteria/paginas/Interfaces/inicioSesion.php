55d1cdc7ae3de915abaf651671f2e83389d5146f
Gianfranco Lombardo López <giandb99@gmail.com>
2024-11-15 21:53:36 +0100

    cree otro boton en el inicio de sesion

--- Proyecto_Ferreteria/paginas/Interfaces/inicioSesion.php
+++ Proyecto_Ferreteria/paginas/Interfaces/inicioSesion.php
@@ -13,9 +13,13 @@
         <h2 class="login-title">Iniciar sesión en Ferretería</h2>
         <input type="hidden" name="accion" value="verificar_usuario">
         <div class="login">
-            <input class="login-input" type="text" name="email" id="email" placeholder="Correo electrónico" required>
-            <input class="login-input" type="password" name="password" id="password" placeholder="Contraseña" required
+            <input class="login-input" type="text" name="email" id="email" placeholder="Correo electrónico">
+            <input class="login-input" type="password" name="password" id="password" placeholder="Contraseña"
                 minlength="6">
+            <div class="admin">
+                <label class="admin-check" for="admin">Continuar como administrador</label>
+                <input type="checkbox" name="admin" id="admin">
+            </div>
             <button class="login-button" type="submit">Iniciar Sesión</button>
         </div>
     </form>
